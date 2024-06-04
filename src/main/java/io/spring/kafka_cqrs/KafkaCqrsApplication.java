0baf5d3864b1096fcae79e9096d313fdb771140f
kihongkim <me@d.com>
2024-06-04 09:21:08 +0900

    first commit

---
+++ src/main/java/io/spring/kafka_cqrs/KafkaCqrsApplication.java
@@ -0,0 +1,13 @@
+package io.spring.kafka_cqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCqrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaCqrsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/spring/kafka_cqrs/KafkaCqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.kafka_cqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaCqrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
